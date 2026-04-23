8cc7a02cfd53dbbed09558908ec37181ed1db58a
unknown <ju.velling@gmail.com>
2026-04-23 19:20:36 +0200

    switched to try catch to prevent an error throwing up

--- backend/db/index.js
+++ backend/db/index.js
@@ -8,15 +8,15 @@ const pool = new Pool({
 });
 
 export const query = (text, params) => {
-    return pool.query(text, params)
-        .catch(err => {
-            console.error('DB ERROR:', {
-                text,
-                params,
-                code: err.code,
-                message: err.message,
-            });
-
-            throw err;
+    try {
+        return pool.query(text, params);
+    } catch (err) {
+        console.error('DB ERROR:', {
+            text,
+            params,
+            code: err.code,
+            message: err.message,
         });
+        throw err;
+    }
 }
