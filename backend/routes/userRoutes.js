8bbe175378513363256cb92dd87c669ac926a3de
unknown <ju.velling@gmail.com>
2026-04-23 19:49:27 +0200

    added requireAuth to /user/new route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 
+import { requireAuth } from '../src/utils.js';
 import userController from '../controllers/userController.js';
 
 const router = express.Router();
 
-router.route('/new').post(userController.newUserPost);
+router.route('/new').post(requireAuth(0), userController.newUserPost);
 
 export default router;
