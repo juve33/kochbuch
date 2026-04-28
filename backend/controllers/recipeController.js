022a46f098695148e3b8b9a9604424ee3c579473
unknown <ju.velling@gmail.com>
2026-04-29 00:27:02 +0200

    created files for recipe api

---
+++ backend/controllers/recipeController.js
@@ -0,0 +1,13 @@
+import bcrypt from 'bcrypt';
+
+import * as db from '../db/index.js';
+
+const allRecipesGet = async (req, res) => {
+
+}
+
+const recipeGet = async (req, res) => {
+    
+}
+
+export default {recipeGet}
---
+++ backend/routes/recipeRoutes.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+import { requireAuth } from '../src/utils.js';
+import recipeController from '../controllers/recipeController.js';
+
+const router = express.Router();
+
+// router.route('/all').get(requireAuth(0), recipeController.allRecipesGet);
+
+// router.route('/new').post(requireAuth(0), recipeController.newRecipePost);
+
+// router.route('/:id').get(requireAuth(0), recipeController.recipeGet);
+// router.route('/:id').post(requireAuth(0), recipeController.recipePost);
+
+export default router;
