bece6f89f01331bd378663019db1fec075e582c7
unknown <ju.velling@gmail.com>
2026-05-05 14:54:56 +0200

    fixed indentation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,11 +3,11 @@ import { Routes, Route } from 'react-router-dom';
 import Login from './features/auth/Login';
 
 function App() {
-  return (
-    <Routes>
-      <Route path="login" element={<Login />} />
-    </Routes>
-  )
+    return (
+        <Routes>
+            <Route path="login" element={<Login />} />
+        </Routes>
+    )
 }
 
 export default App
