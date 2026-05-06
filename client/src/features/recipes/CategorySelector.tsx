14c7340189e670736d1c6f9b7199c5cf1a0126e6
unknown <ju.velling@gmail.com>
2026-05-06 15:51:32 +0200

    feature: added category selector

--- client/src/features/recipes/CategorySelector.tsx
+++ client/src/features/recipes/CategorySelector.tsx
@@ -1,32 +1,51 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-const Login = () => {
+type Category = {
+  id: number;
+  name: string;
+};
+
+const CategorySelector = () => {
+    const [categories, setCategories] = useState<Category[]>([]);
     const [error, setError] = useState("");
 
-    try {
-        const response = await fetch("http://localhost:5000/recipe/categories" , {
-            method: "GET",
-            credentials: "include"
-        });
+    useEffect(() => {
+        const fetchCategories = async () => {
+            try {
+                const response = await fetch("http://localhost:5000/recipe/categories", {
+                method: "GET",
+                credentials: "include",
+                });
 
-        const data = await response.json();
+                const data = await response.json();
 
-        if (!response.ok) {
-            throw new Error(data.error || data.message || "Login failed");
-        }
-    } catch (err) {
-        setError(err.message);
-    }
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        setError("");
-    }
+                if (!response.ok) {
+                    throw new Error(data.error || data.message || "Fetching categories failed");
+                }
+                
+                setCategories(data as Category[]);
+            } catch (err) {
+                const message =
+                    err instanceof Error ? err.message : "Unexpected error";
+                setError(message);
+            }
+        };
+        fetchCategories();
+    }, []);
 
     return (
-        <select>
+        <select disabled={categories.length <= 0}>
+            {
+                error ?
+                    (<p>{error}</p>) :
+                    categories.map((category) => (
+                        <option key={category.id} value={category.id}>
+                            {category.name}
+                        </option>
+                    ))
+            }
         </select>
     )
 }
 
-export default Login
+export default CategorySelector
