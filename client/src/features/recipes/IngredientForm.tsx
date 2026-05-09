17d31b6bee494bf58260b236cfa995aebc262035
unknown <ju.velling@gmail.com>
2026-05-10 00:33:21 +0200

    updated IngredientForm to match StepForm

--- client/src/features/recipes/IngredientForm.tsx
+++ client/src/features/recipes/IngredientForm.tsx
@@ -1,43 +1,101 @@
-import { useState } from 'react';
+import { type JSX } from "react";
+import { useSortable } from '@dnd-kit/sortable';
 
-const IngredientForm = () => {
-    const [amount, setAmount] = useState("");
-    const [unit, setUnit] = useState("");
-    const [text, setText] = useState("");
-    const [comment, setComment] = useState("");
+export type Ingredient = {
+  id: number;
+  amount: number;
+  unit?: string;
+  text: string;
+  comment?: string;
+};
+
+type IngredientFormProps = {
+    value: Ingredient;
+    index: number;
+    setAction: React.Dispatch<React.SetStateAction<Ingredient[]>>;
+    children?: JSX.Element;
+};
+
+const IngredientForm = ({ value, index, setAction, children }: IngredientFormProps) => {
+    const { setNodeRef, attributes, listeners, transform, transition } = useSortable({ id: value.id });
 
     return (
-        <fieldset>
+        <fieldset
+            ref={setNodeRef}
+            style={{
+                transform: transform
+                    ? `translate3d(${transform.x}px, ${transform.y}px, 0)`
+                    : undefined,
+                transition,
+            }}
+        >
+            <button
+                type="button"
+                {...attributes}
+                {...listeners}
+            />
             <input
                 type="number"
                 id="amount"
                 placeholder="500"
-                value={amount}
-                onChange={(e) => setAmount(e.target.value)}
+                onChange={(e) => {
+                    setAction(prev => {
+                        const next = [...prev];
+
+                        prev[index].amount = e.target.valueAsNumber;
+
+                        return next;
+                    });
+                }}
+                value={value.amount}
                 required
             />
             <input
                 type="text"
                 id="unit"
                 placeholder="g"
-                onChange={(e) => setUnit(e.target.value)}
-                value={unit}
-                required
+                onChange={(e) => {
+                    setAction(prev => {
+                        const next = [...prev];
+
+                        prev[index].unit = e.target.value;
+
+                        return next;
+                    });
+                }}
+                value={value.unit}
             />
             <input
                 type="text"
                 id="text"
                 placeholder="Flour"
-                onChange={(e) => setText(e.target.value)}
-                value={text}
+                onChange={(e) => {
+                    setAction(prev => {
+                        const next = [...prev];
+
+                        prev[index].text = e.target.value;
+
+                        return next;
+                    });
+                }}
+                value={value.text}
                 required
             />
             <input
                 type="text"
                 id="comment"
-                onChange={(e) => setComment(e.target.value)}
-                value={comment}
+                onChange={(e) => {
+                    setAction(prev => {
+                        const next = [...prev];
+
+                        prev[index].comment = e.target.value;
+
+                        return next;
+                    });
+                }}
+                value={value.comment}
             />
+            {children}
         </fieldset>
     )
 }
