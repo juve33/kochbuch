ab1cd5d91162314c00e4d64fef4c14621161ad77
unknown <ju.velling@gmail.com>
2026-05-18 23:42:59 +0200

    created empty recipe form and seperate new recipe view

---
+++ client/src/features/recipes/RecipeForm.tsx
@@ -0,0 +1,114 @@
+import React from 'react';
+
+import CategorySelector from './CategorySelector';
+import IngredientForm, { type Ingredient } from './IngredientForm';
+import StepForm, { type Step } from './StepForm';
+import AddButton from '../../components/AddButton';
+import DeleteButton from '../../components/DeleteButton';
+import SortableFieldset from '../../components/SortableFieldset';
+import SortableFieldsetContext from '../../components/SortableFieldsetContext';
+
+import '../../assets/css/recipe.css';
+
+type RecipeFormProps = {
+    nameHook: [string, React.Dispatch<React.SetStateAction<string>>];
+    categoryHook: [string | undefined, React.Dispatch<React.SetStateAction<string | undefined>>];
+    ingredientsHook: [Ingredient[], React.Dispatch<React.SetStateAction<Ingredient[]>>];
+    stepsHook: [Step[], React.Dispatch<React.SetStateAction<Step[]>>];
+    onFormSubmit?: React.SubmitEventHandler<HTMLFormElement>;
+};
+
+const RecipeForm = ({nameHook, categoryHook, ingredientsHook, stepsHook, onFormSubmit }: RecipeFormProps) => {
+    const [name, setName] = nameHook;
+    const [category, setCategory] = categoryHook;
+    const [ingredients, setIngredients] = ingredientsHook;
+    const [steps, setSteps] = stepsHook;
+
+    return (
+        <form onSubmit={onFormSubmit}>
+            <div className='input-group input-group-head'>
+                <input
+                    type="text"
+                    id="recipe-name"
+                    placeholder="New recipe"
+                    onChange={(e) => setName(e.target.value)}
+                    value={name}
+                    required
+                />
+                <CategorySelector onChange={(e) => setCategory(e.target.value)} value={category} />
+            </div>
+            <fieldset className='input-group input-group-ingredients'>
+                <SortableFieldsetContext
+                    value={ingredients}
+                    setAction={setIngredients}
+                >
+                    {ingredients.map((ingredient, index) => (
+                            <SortableFieldset
+                                key={ingredient.id}
+                                id={ingredient.id}
+                                className='input-item input-item-ingredients'
+                            >
+                                <IngredientForm
+                                    index={index}
+                                    value={ingredient}
+                                    setAction={setIngredients}
+                                />
+                                <DeleteButton
+                                    index={index}
+                                    setAction={setIngredients}
+                                >
+                                    x
+                                </DeleteButton>
+                            </SortableFieldset>
+                        ))}
+                </SortableFieldsetContext>
+                <AddButton
+                    createItem={() => ({
+                        id: Date.now(),
+                        text: "",
+                    })}
+                    setAction={setIngredients}
+                >
+                    Add ingredient
+                </AddButton>
+            </fieldset>
+            <fieldset className='input-group input-group-steps'>
+                <SortableFieldsetContext
+                    value={steps}
+                    setAction={setSteps}
+                >
+                    {steps.map((step, index) => (
+                        <SortableFieldset
+                            key={step.id}
+                            id={step.id}
+                            className='input-item input-item-steps'
+                        >
+                            <StepForm
+                                index={index}
+                                value={step}
+                                setAction={setSteps}
+                            />
+                            <DeleteButton
+                                index={index}
+                                setAction={setSteps}
+                            >
+                                x
+                            </DeleteButton>
+                        </SortableFieldset>
+                    ))}
+                </SortableFieldsetContext>
+                <AddButton
+                    createItem={() => ({
+                        id: Date.now(),
+                        text: "",
+                    })}
+                    setAction={setSteps}
+                >
+                    Add step
+                </AddButton>
+            </fieldset>
+        </form>
+    )
+}
+
+export default RecipeForm
--- client/src/views/NewRecipeView.tsx
+++ client/src/views/NewRecipeView.tsx
@@ -1,99 +1,24 @@
 import { useState } from 'react';
 
-import CategorySelector from '../features/recipes/CategorySelector';
-import IngredientForm, { type Ingredient } from '../features/recipes/IngredientForm';
-import StepForm, { type Step } from '../features/recipes/StepForm';
-import AddButton from '../components/AddButton';
-import DeleteButton from '../components/DeleteButton';
-import SortableFieldset from '../components/SortableFieldset';
-import SortableFieldsetContext from '../components/SortableFieldsetContext';
+import RecipeForm from '../features/recipes/RecipeForm';
+import { type Ingredient } from '../features/recipes/IngredientForm';
+import { type Step } from '../features/recipes/StepForm';
 
 const NewRecipeView = () => {
     const [name, setName] = useState<string>("");
     const [category, setCategory] = useState<string>();
-    const [ingredients, setIngredients] = useState<Ingredient[]>([{id: 1, text:""}]);
-    const [steps, setSteps] = useState<Step[]>([{id: 1, text:""}]);
+    const [ingredients, setIngredients] = useState<Ingredient[]>([{id: 1, amountString: "500", unit:"g", text:"Flour"}]);
+    const [steps, setSteps] = useState<Step[]>([{id: 1, text:"In a bowl, mix the flour and the salt"}]);
 
     return (
-        <form>
-            <input
-                type="text"
-                id="recipe-name"
-                placeholder="Cake"
-                onChange={(e) => setName(e.target.value)}
-                value={name}
-                required
+        <>
+            <RecipeForm
+                nameHook={[name, setName]}
+                categoryHook={[category, setCategory]}
+                ingredientsHook={[ingredients, setIngredients]}
+                stepsHook={[steps, setSteps]}
             />
-            <CategorySelector onChange={(e) => setCategory(e.target.value)} value={category} />
-            <fieldset>
-                <SortableFieldsetContext
-                    value={ingredients}
-                    setAction={setIngredients}
-                >
-                    {ingredients.map((ingredient, index) => (
-                            <SortableFieldset
-                                key={ingredient.id}
-                                id={ingredient.id}
-                            >
-                                <IngredientForm
-                                    index={index}
-                                    value={ingredient}
-                                    setAction={setIngredients}
-                                />
-                                <DeleteButton
-                                    index={index}
-                                    setAction={setIngredients}
-                                >
-                                    x
-                                </DeleteButton>
-                            </SortableFieldset>
-                        ))}
-                </SortableFieldsetContext>
-                <AddButton
-                    createItem={() => ({
-                        id: Date.now(),
-                        text: "",
-                    })}
-                    setAction={setIngredients}
-                >
-                    Add ingredient
-                </AddButton>
-            </fieldset>
-            <fieldset>
-                <SortableFieldsetContext
-                    value={steps}
-                    setAction={setSteps}
-                >
-                    {steps.map((step, index) => (
-                        <SortableFieldset
-                            key={step.id}
-                            id={step.id}
-                        >
-                            <StepForm
-                                index={index}
-                                value={step}
-                                setAction={setSteps}
-                            />
-                            <DeleteButton
-                                index={index}
-                                setAction={setSteps}
-                            >
-                                x
-                            </DeleteButton>
-                        </SortableFieldset>
-                    ))}
-                </SortableFieldsetContext>
-                <AddButton
-                    createItem={() => ({
-                        id: Date.now(),
-                        text: "",
-                    })}
-                    setAction={setSteps}
-                >
-                    Add step
-                </AddButton>
-            </fieldset>
-        </form>
+        </>
     )
 }
 
