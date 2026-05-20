ad9857d15075666f268bf48aa9d06fdb75325313
unknown <ju.velling@gmail.com>
2026-05-20 19:08:35 +0200

    exported pool aswell

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,15 +1,15 @@
 import { Pool } from 'pg';
 
-const pool = new Pool({
+export const pool = new Pool({
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     host: process.env.DATABASE_HOST,
     database: process.env.DATABASE_DB,
 });
 
-export const query = (text, params) => {
+export const query = async (text, params) => {
     try {
-        return pool.query(text, params);
+        return await pool.query(text, params);
     } catch (err) {
         console.error('DB ERROR:', {
             text,
