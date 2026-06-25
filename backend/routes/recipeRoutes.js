dfe1129ef863fc55e3c85234d7f9c381ea205b24
unknown <ju.velling@gmail.com>
2026-06-25 23:40:19 +0200

    added necessary recipe routes as comments

--- backend/routes/recipeRoutes.js
+++ backend/routes/recipeRoutes.js
@@ -8,9 +8,11 @@ const router = express.Router();
 router.route('/').get(requireAuth(0), recipeController.allRecipesGet);
 router.route('/').post(requireAuth(0), recipeController.newRecipePost);
 
+//router.route('/categories').delete(requireAuth(0), recipeController.categoriesDelete);
 router.route('/categories').get(requireAuth(0), recipeController.categoriesGet);
 router.route('/categories').post(requireAuth(0), recipeController.categoriesPost);
 
+//router.route('/:id').delete(requireAuth(0), recipeController.recipeDelete);
 router.route('/:id').get(requireAuth(0), recipeController.recipeGet);
 router.route('/:id').post(requireAuth(0), recipeController.recipePost);
 
