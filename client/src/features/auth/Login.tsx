4297a676b5607674312df851372130dfcf72c23a
unknown <ju.velling@gmail.com>
2026-06-30 15:42:21 +0200

    fix: removed global state from login since it isn't available outside the logged in area

--- client/src/features/auth/Login.tsx
+++ client/src/features/auth/Login.tsx
@@ -1,10 +1,6 @@
 import React, { useState } from 'react';
 
-import { useGlobalStateDispatch } from '../../utils/GlobalState';
-
 const Login = () => {
-    const dispatchGlobalState = useGlobalStateDispatch();
-    
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [loading, setLoading] = useState(false);
@@ -31,10 +27,6 @@ const Login = () => {
                 throw new Error(data.error || data.message || "Login failed");
             }
 
-            dispatchGlobalState({
-                type: "set username",
-                username: username
-            });
             window.location.href = "/overview";
         } catch (err) {
             const message =
