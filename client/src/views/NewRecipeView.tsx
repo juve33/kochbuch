f72523641c9ea5350e6d99778c48ae96aeb0856c
unknown <ju.velling@gmail.com>
2026-07-02 01:26:11 +0200

    fix: fixed type error

--- client/src/views/NewRecipeView.tsx
+++ client/src/views/NewRecipeView.tsx
@@ -45,6 +45,7 @@ const NewRecipeView = () => {
                 context={{
                     recipe: {
                         name: "",
+                        role: 10,
                         ingredients: [{index_number: 0, amount: 500, unit:"g", text:"Flour"}],
                         steps: [{index_number: 0, text:"In a bowl, mix the flour and the salt"}]
                     },
