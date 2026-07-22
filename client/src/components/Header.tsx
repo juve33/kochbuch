42d1ccecdcdcf17bdf5530a87f7b761825eb604d
unknown <ju.velling@gmail.com>
2026-07-22 22:49:35 +0200

    feature: added username to page title

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState, type JSX } from "react";
 import { createPortal } from "react-dom";
 
-import { useGlobalStateDispatch } from "../utils/GlobalState";
+import { useGlobalState, useGlobalStateDispatch } from "../utils/GlobalState";
 
 import '../assets/css/header.css';
 
@@ -10,6 +10,7 @@ type HeaderProps = {
 };
 
 const Header = ({ children } : HeaderProps) => {
+    const globalState = useGlobalState();
     const dispatchGlobalState = useGlobalStateDispatch();
 
     const headerMainRef = useRef<HTMLDivElement | null>(null);
@@ -26,18 +27,21 @@ const Header = ({ children } : HeaderProps) => {
     }, []);
 
     return createPortal(
-        <header className="header__wrapper">
-            <div className="header__inner">
-                <div className="header__portal-main" ref={headerMainRef}></div>
-                <div className="header__content">
-                    {children}
+        <>
+            <title>{globalState.user?.name + "s Kochbuch"}</title>
+            <header className="header__wrapper">
+                <div className="header__inner">
+                    <div className="header__portal-main" ref={headerMainRef}></div>
+                    <div className="header__content">
+                        {children}
+                    </div>
+                    <div className="header__portal-menu">
+                        <button type="button" className="menu-button" onClick={() => setMenuOpen(!menuOpen)}></button>
+                        <div className="header__portal-menu__items" ref={headerMenuRef} aria-expanded={menuOpen} onClick={() => setMenuOpen(false)}></div>
+                    </div>
                 </div>
-                <div className="header__portal-menu">
-                    <button type="button" className="menu-button" onClick={() => setMenuOpen(!menuOpen)}></button>
-                    <div className="header__portal-menu__items" ref={headerMenuRef} aria-expanded={menuOpen} onClick={() => setMenuOpen(false)}></div>
-                </div>
-            </div>
-        </header>,
+            </header>
+        </>,
         document.body
     )
 }
