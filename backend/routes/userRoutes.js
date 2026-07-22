4c51230d0d5a982e2b42be8852e5d483a58a5fb6
unknown <ju.velling@gmail.com>
2026-07-23 00:23:35 +0200

    fix: made admin role a requirement for creating new users

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,11 +5,11 @@ import userController from '../controllers/userController.js';
 
 const router = express.Router();
 
+router.route('/').post(requireAuth(10), userController.newUserPost);
+
 router.route('/share').get(requireAuth(0), userController.innerApiKeysGet);
 
 router.route('/me').get(requireAuth(0), userController.meGet);
 router.route('/me').post(requireAuth(0), userController.mePost);
-
-router.route('/new').post(requireAuth(0), userController.newUserPost);
 
 export default router;
